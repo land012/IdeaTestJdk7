0fdada6e31da45779954b5c6db6f7e69b06c75d9
land012 <shin.12@163.com>
2015-07-22 22:20:21 +0800

    merger git test2

---
+++ appDemo/src/main/java/com/umbrella/demo/git/GitDemo.java
@@ -0,0 +1,13 @@
+package com.umbrella.demo.git;
+
+import org.junit.Test;
+
+/**
+ * Created by xudazhou on 2015/7/22.
+ */
+public class GitDemo {
+    @Test
+    public void test1() {
+        System.out.println("2");
+    }
+}
