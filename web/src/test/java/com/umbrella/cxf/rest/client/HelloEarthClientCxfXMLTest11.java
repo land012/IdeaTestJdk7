8f3339e30da31719b6ff534fbc73d93818d0a9fb
shin012 <shin.12@163.com>
2015-10-21 15:19:37 +0800

    REST Http add

---
+++ web/src/test/java/com/umbrella/cxf/rest/client/HelloEarthClientCxfXMLTest11.java
@@ -0,0 +1,48 @@
+package com.umbrella.cxf.rest.client;
+
+import com.umbrella.cxf.rest.service.HelloEarthService;
+import com.umbrella.cxf.ws.server.HelloWorldService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by xudazhou on 2015/8/14.
+ * cxf 配置文件1 spring-config-rest-client.xml
+ */
+public class HelloEarthClientCxfXMLTest11 {
+    private HelloEarthService helloEarthService;
+    private HelloWorldService helloWorldService;
+
+    public void setHelloEarthService(HelloEarthService helloEarthService) {
+        this.helloEarthService = helloEarthService;
+    }
+
+    public void setHelloWorldService(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @Before
+    public void before() {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{ "spring-config-rest-client.xml" });
+        context.getAutowireCapableBeanFactory().autowireBeanProperties(this, DefaultListableBeanFactory.AUTOWIRE_BY_NAME, false);
+    }
+
+    /**
+     * 测试 CXF REST
+     */
+    @Test
+    public void test1() {
+        System.out.println(this.helloEarthService.getStudentById(1));
+    }
+
+    /**
+     * 验证 Spring
+     */
+    @Test
+    public void test2() {
+        System.out.println(this.helloWorldService.sayHello("Hatake"));
+    }
+}
