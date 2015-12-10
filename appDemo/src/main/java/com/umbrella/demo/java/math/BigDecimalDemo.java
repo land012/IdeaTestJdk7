bce7b27c57cb0e5dec466dcfcbbf2fd8297ba5eb
shin012 <shin.12@163.com>
2015-12-10 10:21:44 +0800

    bigdecimal equals

--- appDemo/src/main/java/com/umbrella/demo/java/math/BigDecimalDemo.java
+++ appDemo/src/main/java/com/umbrella/demo/java/math/BigDecimalDemo.java
@@ -14,4 +14,21 @@ public class BigDecimalDemo {
         System.out.println(bd1);
         System.out.println(bd1.abs());
     }
+
+    @Test
+    public void test2() {
+        BigDecimal b1 = new BigDecimal(0);
+        BigDecimal b2 = new BigDecimal(0.000000);
+        BigDecimal b3 = new BigDecimal("0.000000");
+
+        System.out.println(b1); // 0
+        System.out.println(b2); // 0
+        System.out.println(b3); // 0.000000
+
+        System.out.println(b1.equals(b2)); // true
+        System.out.println(BigDecimal.ZERO.equals(b1)); // true
+        System.out.println(BigDecimal.ZERO.equals(b2)); // true
+        System.out.println(b1.equals(b3)); // false
+        System.out.println(BigDecimal.ZERO.equals(b3)); // false
+    }
 }
