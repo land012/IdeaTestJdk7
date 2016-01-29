93aa087c8caca809fa89091924701607996cf304
shin012 <shin.12@163.com>
2016-01-29 15:19:55 +0800

    根据类获取路径

--- appDemo/src/main/java/com/umbrella/demo/java/demo/ClasspathDemo.java
+++ appDemo/src/main/java/com/umbrella/demo/java/demo/ClasspathDemo.java
@@ -1,6 +1,9 @@
 package com.umbrella.demo.java.demo;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import java.net.URL;
 
 /**
  * Created by xudazhou on 2015/8/25.
@@ -26,5 +29,20 @@ public class ClasspathDemo {
         // 所以 jar路径
         String java_class_path = System.getProperty("java.class.path");
         System.out.println(java_class_path);
+    }
+
+    /**
+     * 类所在 jar文件 的路径
+     */
+    @Test
+    public void test2() {
+        URL location = StringUtils.class.getProtectionDomain().getCodeSource().getLocation();
+        System.out.println(location.getPath()); // /D:/_mvn_repository/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar
+        System.out.println(location.getFile()); // /D:/_mvn_repository/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar
+
+        // file:/D:/_mvn_repository/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar!/org/apache/commons/lang3/
+        System.out.println(StringUtils.class.getResource("").getPath());
+        // /D:/_idea/TestApp/appDemo/target/classes/
+        System.out.println(StringUtils.class.getResource("/").getPath());
     }
 }
