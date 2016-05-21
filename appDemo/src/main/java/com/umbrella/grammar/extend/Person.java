869d223b6c3543ca948b3ee5b1bba7fe4959484c
shin012 <shin.12@163.com>
2016-05-21 11:26:08 +0800

    继承方法调用 2

--- appDemo/src/main/java/com/umbrella/grammar/extend/Person.java
+++ appDemo/src/main/java/com/umbrella/grammar/extend/Person.java
@@ -1,9 +1,15 @@
 package com.umbrella.grammar.extend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by 大洲 on 14-12-24.
  */
 public class Person {
+
+    private static final Logger log = LoggerFactory.getLogger(Person.class);
+
     private String name;
 
     public String getName() {
@@ -12,5 +18,13 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void perform() {
+        run();
+    }
+
+    public void run() {
+        log.info("I am running");
     }
 }
--- appDemo/src/main/java/com/umbrella/grammar/extend/Student.java
+++ appDemo/src/main/java/com/umbrella/grammar/extend/Student.java
@@ -1,9 +1,15 @@
 package com.umbrella.grammar.extend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by 大洲 on 14-12-24.
  */
 public class Student extends Person {
+
+    private static final Logger log = LoggerFactory.getLogger(Student.class);
+
     private String name;
 
     public String getName() {
@@ -12,5 +18,10 @@ public class Student extends Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void run() {
+        log.info("I am running");
     }
 }
