05fe6eb1a7ee9f3b6022fcfdb347d2f50fa1a0f0
shin012 <shin.12@163.com>
2016-12-16 16:28:18 +0800

    rebase demo1

---
+++ appDemo/src/main/java/com/umbrella/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.umbrella.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by xudazhou on 2016/12/16.
+ */
+public class HelloWorld {
+    @Test
+    public void test1() {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
