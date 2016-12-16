45dafb34913543226510b77f4f5c9d70e5c8e88b
shin012 <shin.12@163.com>
2016-12-16 16:32:21 +0800

    rebase demo 4

--- appDemo/src/main/java/com/umbrella/demo/HelloWorld.java
+++ appDemo/src/main/java/com/umbrella/demo/HelloWorld.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class HelloWorld {
     @Test
     public void test1() {
-        System.out.println(3);
-        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
     }
 }
